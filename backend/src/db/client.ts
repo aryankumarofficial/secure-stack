a4484d065f2d3d80351adaf47999266d9d0e5048
Aryan Kumar <aryanak9163@gmail.com>
2026-05-10 18:21:37 +0530

    feat(db): writes the prisma client

---
+++ backend/src/db/client.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "../.generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL required for this operation");
+}
+
+const pool = new Pool({ connectionString });
+
+const adapter = new PrismaPg(pool);
+
+const client = new PrismaClient({
+  adapter,
+});
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma = global.prisma ?? client;
+
+if (process.env.NODE_ENV === "development") global.prisma = client;
