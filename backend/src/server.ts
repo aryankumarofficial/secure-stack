8603ee3b8cf452eb8b311a9f65dd1e7b6b4f5b98
Aryan Kumar <aryanak9163@gmail.com>
2026-05-17 05:17:48 +0530

    feat: adds fallback port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,16 +6,17 @@ import logger from "./middlewares/logger.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import rootRouter from "./routes/index.js";
 import swaggerDocument from "./swagger.json";
+
 const app = express();
-const port = Number(process.env.PORT);
+const port = Number(process.env.PORT) || 5000;
 app.use(
-  cors({
-    origin: process.env.FRONTEND_ORIGIN,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    optionsSuccessStatus: 200,
-    credentials: true,
-  }),
+    cors({
+        origin: process.env.FRONTEND_ORIGIN,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        optionsSuccessStatus: 200,
+        credentials: true,
+    }),
 );
 
 app.use(express.json());
@@ -24,10 +25,10 @@ app.use(cookieParser());
 app.use(logger);
 
 app.get("/", (_, res) => {
-  return res.json({
-    status: "OK",
-    message: "Server Running Successfully",
-  });
+    return res.json({
+        status: "OK",
+        message: "Server Running Successfully",
+    });
 });
 
 app.use("/api", rootRouter);
@@ -35,6 +36,6 @@ app.use("/api", rootRouter);
 app.use(errorMiddleware);
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.listen(port, () => {
-  console.log(`Server listening at ${port}`);
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server listening at ${port}`);
 });
